b7b26336e7e6bbb799381b169eec93ed719c47ba
COLLIN PAGE <COLLIN.PAGE@bvk.com>
2022-03-07 12:16:36 -0600

    finished answering question four

--- Week_One/queries.sql
+++ Week_One/queries.sql
@@ -32,6 +32,24 @@ GROUP BY customer_id, order_date, product_name;
 
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+WITH counts AS (
+SELECT COUNT(s.product_id), m.product_name
+FROM dannys_diner.sales AS s
+JOIN dannys_diner.menu AS m
+ON m.product_id = s.product_id
+GROUP BY m.product_name)
+-- creating a table here that counts the amount of orders each product has
+SELECT product_name, MAX(count) AS most_orders -- creating a new column name 'most_orders'
+FROM counts
+WHERE count=(
+  SELECT MAX(count) FROM counts) -- only returning results that match the column value of the max
+GROUP BY counts.product_name
+ 
+-- OR
+
+
+
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
 -- 7. Which item was purchased just before the customer became a member?
